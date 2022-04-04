aa4d34ef1ff327ec82ab4eb60dd93874978d697b
Cosmin Vulpe <cosmin98vulpe@gmail.com>
2022-04-04 13:13:31 +0300

    Add footer

--- src/main/java/com/codecool/hackernews/views/Layout.java
+++ src/main/java/com/codecool/hackernews/views/Layout.java
@@ -8,6 +8,7 @@ public class Layout {
                 "<meta charset=\"UTF-8\">\n" +
                 "<title>" + titlePage + "</title>\n" +
                 "<link rel=\"stylesheet\" type=\"text/css\" href='/static/css/site.css' />\n" +
+                "<link rel=\"stylesheet\" href=\"path/to/bootstrap/css/bootstrap.min.css\">\n" +
                 "<script src=\"/static/js/main.js\" defer>" + "</script>\n" +
                 "</head>";
     }
@@ -19,31 +20,40 @@ public class Layout {
                 "<h2 align = \"center\">" + title + "</h2>\n" +
                 "<nav>\n" +
                 "<ul class=\"nav-links\">\n" +
-                "<li><a href=\"#\">Top news</a></li>\n" +
-                "<li><a href=\"#\">Newest</a></li>\n" +
-                "<li><a href=\"#\">Jobs</a></li>\n" +
+                "<li><a href=\"#\" id='top-news-header'>Top news</a></li>\n" +
+                "<li><a href=\"#\" id='newest-header'>Newest</a></li>\n" +
+                "<li><a href=\"#\"id='jobs-header'>Jobs</a></li>\n" +
                 "<li><a href=\"#\">Curriculum</a></li>\n" +
                 "</ul>\n" +
                 "</nav>\n" +
                 "<a class=\"cta\" href=\"#\"><button>Sign up</button></a>\n" +
                 "<a class=\"cta\" href=\"#\"><button>Login</button></a>\n" +
                 "</header>\n";
-
     }
 
     public String getMainBodyHtml() {
-        return "<div id='top-news'></div>\n"
-                ;
-
+        return "<div id='news'></div>\n" +
+                "<div id='newest'></div>\n" +
+                "<div id='jobs'></div>\n";
     }
 
     public String getFooterHtml() {
         return "<footer>\n" +
-                "<p></p>\n" +
+                "<div class='footer-content'>\n" +
+                "<ul class='socials'>" +
+                "<li><a href='#'><i class='fa fa-facebook-official' aria-hidden='true'></i></a></li>\n" +
+                "<li><a href='#'><i class='fa fa-twitter' aria-hidden='true'></i></a></li>\n" +
+                "<li><a href='#'><i class='fa fa-github' aria-hidden='true'></i></a></li>\n" +
+                "<li><a href='#'><i class='fa fa-linkedin-square' aria-hidden='true'></i></a></li>\n" +
+                "</ul>" +
+                "</div>" +
+                "<div class='footer-bottom'>\n"+
+                "<p>copyright &copy;2022  designed by Cosmin Vulpe</p>\n" +
+                "<p>email: cosmin98vulpe@gmail.com</p>\n" +
+                "<li><a href='#'><span class=\"fa-brands fa-github\"></span></i></a></li>\n" +
+                "</div>\n"+
                 "</footer>\n" +
                 "</body>\n" +
                 "</html>";
     }
-
-
 }
--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,15 +1,18 @@
-console.log("test");
-let page = 1;
+let pageNews = 1;
+let pageNewest = 1;
+let pageJobs = 1;
+const topNews = document.querySelector('#top-news-header');
+const newest = document.querySelector('#newest-header');
+const jobs = document.querySelector('#jobs-header');
 
 
-const getInfo = async () => {
-    const divMain = document.querySelector("#top-news");
+const getInfo = async (fetchUrl, divQuery, page) => {
+    const divMain = document.querySelector(divQuery);
     let result = "";
-    const request = await fetch('/api/top?page=' + page);
+    const request = await fetch(fetchUrl + page);
     const res = await request.json();
-    console.log(res)
     for (let i = 0; i < res.length; i++) {
-        result += '<div class="card" >' + '<p>' + `<a href='${res[i]['url']}'>` + res[i]['title'] + '</a>' + '</p>';
+        result += '<div class="card" >' + '<p>' + `<a href='${res[i]['url']}' target='_blank'>` + res[i]['title'] + '</a>' + '</p>';
         result += '<br>';
         result += '<p>' + res[i]['time_ago'] + '</p>';
         result += '<br>';
@@ -18,9 +21,30 @@ const getInfo = async () => {
     divMain.innerHTML = result;
 }
 
+const deletePreviousDiv = (divDelete) => {
+    const div = document.querySelector(divDelete);
+    // div.style("<i class='fa fa-spinner fa-pulse fa-3x fa-fw'</i>");
+    div.innerHTML = '';
+
+}
 
 const init = async () => {
-    getInfo();
+    await getInfo("/api/top?page=", '#news', pageNews)
+    topNews.addEventListener('click', () => {
+        deletePreviousDiv('#newest');
+        deletePreviousDiv('#jobs');
+        getInfo("/api/top?page=", '#news', pageNews)
+    });
+    newest.addEventListener('click', () => {
+        deletePreviousDiv('#news');
+        deletePreviousDiv('#jobs');
+        getInfo("/api/newest?page=", '#newest', pageNewest)
+    })
+    jobs.addEventListener('click', () => {
+        deletePreviousDiv('#news');
+        deletePreviousDiv('#newest');
+        getInfo("/api/jobs?page=", '#jobs', pageJobs);
+    })
 }
 init().then()
 
