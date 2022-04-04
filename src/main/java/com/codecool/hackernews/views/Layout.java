ab2a8b18df93d2bc0df25d0c21110d6bee2cd245
Cosmin Vulpe <cosmin98vulpe@gmail.com>
2022-04-04 16:04:02 +0300

    Add pagination

--- src/main/java/com/codecool/hackernews/views/Layout.java
+++ src/main/java/com/codecool/hackernews/views/Layout.java
@@ -26,33 +26,35 @@ public class Layout {
                 "<li><a href=\"#\">Curriculum</a></li>\n" +
                 "</ul>\n" +
                 "</nav>\n" +
-                "<a class=\"cta\" href=\"#\"><button>Sign up</button></a>\n" +
-                "<a class=\"cta\" href=\"#\"><button>Login</button></a>\n" +
+                "<a class=\"cta\" href=\"#footer\"><button>Contact</button></a>\n" +
                 "</header>\n";
     }
 
     public String getMainBodyHtml() {
         return "<div id='news'></div>\n" +
                 "<div id='newest'></div>\n" +
-                "<div id='jobs'></div>\n";
+                "<div id='jobs'></div>\n" +
+                "<div id='pagination'></div>\n";
     }
 
     public String getFooterHtml() {
-        return "<footer>\n" +
-                "<div class='footer-content'>\n" +
-                "<ul class='socials'>" +
-                "<li><a href='#'><i class='fa fa-facebook-official' aria-hidden='true'></i></a></li>\n" +
-                "<li><a href='#'><i class='fa fa-twitter' aria-hidden='true'></i></a></li>\n" +
-                "<li><a href='#'><i class='fa fa-github' aria-hidden='true'></i></a></li>\n" +
-                "<li><a href='#'><i class='fa fa-linkedin-square' aria-hidden='true'></i></a></li>\n" +
-                "</ul>" +
-                "</div>" +
-                "<div class='footer-bottom'>\n"+
-                "<p>copyright &copy;2022  designed by Cosmin Vulpe</p>\n" +
-                "<p>email: cosmin98vulpe@gmail.com</p>\n" +
-                "<li><a href='#'><span class=\"fa-brands fa-github\"></span></i></a></li>\n" +
-                "</div>\n"+
-                "</footer>\n" +
+            return "<footer>\n" +
+                        "<section id='footer'>"+
+                            "<div class='footer-content'>\n" +
+                                "<ul class='socials'>" +
+                                    "<li><a href='#'><i class='fa fa-facebook-official' aria-hidden='true'></i></a></li>\n" +
+                                    "<li><a href='#'><i class='fa fa-twitter' aria-hidden='true'></i></a></li>\n" +
+                                    "<li><a href='#'><i class='fa fa-github' aria-hidden='true'></i></a></li>\n" +
+                                    "<li><a href='#'><i class='fa fa-linkedin-square' aria-hidden='true'></i></a></li>\n" +
+                                "</ul>" +
+                            "</div>" +
+                            "<div class='footer-bottom'>\n"+
+                                "<p>copyright &copy;2022  designed by Cosmin Vulpe</p>\n" +
+                                "<p>email: cosmin98vulpe@gmail.com</p>\n" +
+                                "<li><a href='#'><span class=\"fa-brands fa-github\"></span></i></a></li>\n" +
+                            "</div>\n"+
+                    "</section>"+
+                    "</footer>\n" +
                 "</body>\n" +
                 "</html>";
     }
